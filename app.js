66e056aab5820683c8feeb9d2ba50632359e7fe9
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-07-12 15:46:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//                             Question: 01
+
+/* function greeting() {
+    alert("Hello")
+}
+greeting() */
+
+//                             Question: 02
+
+/* function mobile1() {
+    alert("This is iphone6plus")
+}
+function mobile2() {
+    alert("This is Galaxys6EdgePlus")
+}
+function mobile1() {
+    alert("This is v10")
+}
+function mobile1() {
+    alert("This is Huawenp8")
+} */
+
+//                            Question: 03
+
+/* function changeImage(image) {
+    image.src = "image2.jpg"; 
+  }
+  function resetImage(image) {
+    image.src = "myimage.jpg"; 
+} */
+
+//                            Question: 05
+
+/* var counterValue = 0;
+function increaseCounter() {
+    counterValue++;
+    displayCounter();
+}
+function decreaseCounter() {
+    if (counterValue > 0) {
+        counterValue--;
+        displayCounter();
+    }
+}
+function displayCounter() {
+    var counterElement = document.getElementById("counter");
+    counterElement.textContent = counterValue; 
+} */
